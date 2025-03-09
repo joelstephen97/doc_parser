b9783507c678e768782ee9f4ed4e83c5d5b269b5
Joel Stephen <jojostev@gmail.com>
2025-03-09 04:43:59 +0400

    author

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="doc_parser",
     version="0.1.0",
     description="A powerful document parser for resumes and general documents.",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Joel Stephen",
+    author_email="jojostev@gmail.com",
     packages=find_packages(),
     python_requires=">=3.6",
     classifiers=[
